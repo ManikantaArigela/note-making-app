b81d6a184004776725a67d87b1b2a87dd1176564
Manikanta <arigelamanikanta2005@gmail.com>
2026-09-20 22:01:49 +0530

    changed the url on the axiosCLient file

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITe_API_BASE_URL  || '/api',
   headers: {
     'Content-Type': 'application/json',
   },
