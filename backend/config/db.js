55faf66d60a1354c681bc828b3c990ab4b062056
Manikanta <arigelamanikanta2005@gmail.com>
2026-09-20 14:04:58 +0530

    changed the database file with the perfect uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/productivity_app');
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb:mongodb+srv://arigelamanikanta2006_db_user:pushpa@cluster0.bwfmbxs.mongodb.net/?appName=Cluster0');
     console.log(`[MongoDB Connected]: ${conn.connection.host}`);
   } catch (error) {
     console.error(`[MongoDB Connection Error]: ${error.message}`);
